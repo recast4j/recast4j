14f2fee4b52b5b03d8429cbe878316a9dd23ae69
Piotr Piastucki <piotr@jtilia.org>
2018-02-04 23:37:49 +0100

    Store number of verts per poly in file and bump nav mesh file version

--- detour/src/main/java/org/recast4j/detour/io/MeshSetReader.java
+++ detour/src/main/java/org/recast4j/detour/io/MeshSetReader.java
@@ -31,80 +31,101 @@ import org.recast4j.detour.NavMeshParams;
 
 public class MeshSetReader {
 
-	private final MeshDataReader meshReader = new MeshDataReader();
-	private final NavMeshParamReader paramReader = new NavMeshParamReader();
+    private final MeshDataReader meshReader = new MeshDataReader();
+    private final NavMeshParamReader paramReader = new NavMeshParamReader();
 
-	public NavMesh read(InputStream is, int maxVertPerPoly) throws IOException {
-		ByteBuffer bb = IOUtils.toByteBuffer(is);
-		return read(bb, maxVertPerPoly, false);
-	}
+    public NavMesh read(InputStream is, int maxVertPerPoly) throws IOException {
+        return read(IOUtils.toByteBuffer(is), maxVertPerPoly, false);
+    }
 
-	public NavMesh read(ByteBuffer bb, int maxVertPerPoly) throws IOException {
-		return read(bb, maxVertPerPoly, false);
-	}
+    public NavMesh read(ByteBuffer bb, int maxVertPerPoly) throws IOException {
+        return read(bb, maxVertPerPoly, false);
+    }
 
-	public NavMesh read32Bit(InputStream is, int maxVertPerPoly) throws IOException {
-		ByteBuffer bb = IOUtils.toByteBuffer(is);
-		return read(bb, maxVertPerPoly, true);
-	}
+    public NavMesh read32Bit(InputStream is, int maxVertPerPoly) throws IOException {
+        return read(IOUtils.toByteBuffer(is), maxVertPerPoly, true);
+    }
 
-	public NavMesh read32Bit(ByteBuffer bb, int maxVertPerPoly) throws IOException {
-		return read(bb, maxVertPerPoly, true);
-	}
+    public NavMesh read32Bit(ByteBuffer bb, int maxVertPerPoly) throws IOException {
+        return read(bb, maxVertPerPoly, true);
+    }
 
-	NavMesh read(ByteBuffer bb, int maxVertPerPoly, boolean is32Bit) throws IOException {
-		NavMeshSetHeader header = new NavMeshSetHeader();
-		header.magic = bb.getInt();
-		if (header.magic != NavMeshSetHeader.NAVMESHSET_MAGIC) {
-			header.magic = IOUtils.swapEndianness(header.magic);
-			if (header.magic != NavMeshSetHeader.NAVMESHSET_MAGIC) {
-				throw new IOException("Invalid magic");
-			}
-			bb.order(bb.order() == ByteOrder.BIG_ENDIAN ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
-		}
-		header.version = bb.getInt();
-		if (header.version != NavMeshSetHeader.NAVMESHSET_VERSION) {
-			if (header.version != NavMeshSetHeader.NAVMESHSET_VERSION_RECAST4J) {
-				throw new IOException("Invalid version");
-			}
-		}
-		boolean cCompatibility = header.version == NavMeshSetHeader.NAVMESHSET_VERSION;
-		header.numTiles = bb.getInt();
-		header.params = paramReader.read(bb);
-		NavMesh mesh = new NavMesh(header.params, maxVertPerPoly);
+    public NavMesh read(InputStream is) throws IOException {
+        return read(IOUtils.toByteBuffer(is));
+    }
 
-		// Read tiles.
-		for (int i = 0; i < header.numTiles; ++i) {
-			NavMeshTileHeader tileHeader = new NavMeshTileHeader();
-			if (is32Bit) {
-				tileHeader.tileRef = convert32BitRef(bb.getInt(), header.params);
-			} else {
-				tileHeader.tileRef = bb.getLong();
-			}
-			tileHeader.dataSize = bb.getInt();
-			if (tileHeader.tileRef == 0 || tileHeader.dataSize == 0) {
-				break;
-			}
-			if (cCompatibility && !is32Bit) {
-				bb.getInt(); // C struct padding
-			}
-			MeshData data = meshReader.read(bb, mesh.getMaxVertsPerPoly(), is32Bit);
-			mesh.addTile(data, i, tileHeader.tileRef);
-		}
-		return mesh;
-	}
+    public NavMesh read(ByteBuffer bb) throws IOException {
+        return read(bb, -1, false);
+    }
 
-	private long convert32BitRef(int ref, NavMeshParams params) {
-		int m_tileBits = ilog2(nextPow2(params.maxTiles));
-		int m_polyBits = ilog2(nextPow2(params.maxPolys));
-		// Only allow 31 salt bits, since the salt mask is calculated using 32bit uint and it will overflow.
-		int m_saltBits = Math.min(31, 32 - m_tileBits - m_polyBits);
-		int saltMask = (1 << m_saltBits) - 1;
-		int tileMask = (1 << m_tileBits) - 1;
-		int polyMask = (1 << m_polyBits) - 1;
-		int salt = ((ref >> (m_polyBits + m_tileBits)) & saltMask);
-		int it = ((ref >> m_polyBits) & tileMask);
-		int ip = ref & polyMask;
-		return NavMesh.encodePolyId(salt, it, ip);
-	}
+    NavMesh read(ByteBuffer bb, int maxVertPerPoly, boolean is32Bit) throws IOException {
+        NavMeshSetHeader header = readHeader(bb, maxVertPerPoly);
+        if (header.maxVertsPerPoly <= 0) {
+            throw new IOException("Invalid number of verts per poly " + header.maxVertsPerPoly);
+        }
+        boolean cCompatibility = header.version == NavMeshSetHeader.NAVMESHSET_VERSION;
+        NavMesh mesh = new NavMesh(header.params, header.maxVertsPerPoly);
+        readTiles(bb, is32Bit, header, cCompatibility, mesh);
+        return mesh;
+    }
+
+    private NavMeshSetHeader readHeader(ByteBuffer bb, int maxVertsPerPoly) throws IOException {
+        NavMeshSetHeader header = new NavMeshSetHeader();
+        header.magic = bb.getInt();
+        if (header.magic != NavMeshSetHeader.NAVMESHSET_MAGIC) {
+            header.magic = IOUtils.swapEndianness(header.magic);
+            if (header.magic != NavMeshSetHeader.NAVMESHSET_MAGIC) {
+                throw new IOException("Invalid magic " + header.magic);
+            }
+            bb.order(bb.order() == ByteOrder.BIG_ENDIAN ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
+        }
+        header.version = bb.getInt();
+        if (header.version != NavMeshSetHeader.NAVMESHSET_VERSION && header.version != NavMeshSetHeader.NAVMESHSET_VERSION_RECAST4J_1
+                && header.version != NavMeshSetHeader.NAVMESHSET_VERSION_RECAST4J) {
+            throw new IOException("Invalid version " + header.version);
+        }
+        header.numTiles = bb.getInt();
+        header.params = paramReader.read(bb);
+        header.maxVertsPerPoly = maxVertsPerPoly;
+        if (header.version == NavMeshSetHeader.NAVMESHSET_VERSION_RECAST4J) {
+            header.maxVertsPerPoly = bb.getInt();
+        }
+        return header;
+    }
+
+    private void readTiles(ByteBuffer bb, boolean is32Bit, NavMeshSetHeader header, boolean cCompatibility, NavMesh mesh)
+            throws IOException {
+        // Read tiles.
+        for (int i = 0; i < header.numTiles; ++i) {
+            NavMeshTileHeader tileHeader = new NavMeshTileHeader();
+            if (is32Bit) {
+                tileHeader.tileRef = convert32BitRef(bb.getInt(), header.params);
+            } else {
+                tileHeader.tileRef = bb.getLong();
+            }
+            tileHeader.dataSize = bb.getInt();
+            if (tileHeader.tileRef == 0 || tileHeader.dataSize == 0) {
+                break;
+            }
+            if (cCompatibility && !is32Bit) {
+                bb.getInt(); // C struct padding
+            }
+            MeshData data = meshReader.read(bb, mesh.getMaxVertsPerPoly(), is32Bit);
+            mesh.addTile(data, i, tileHeader.tileRef);
+        }
+    }
+
+    private long convert32BitRef(int ref, NavMeshParams params) {
+        int m_tileBits = ilog2(nextPow2(params.maxTiles));
+        int m_polyBits = ilog2(nextPow2(params.maxPolys));
+        // Only allow 31 salt bits, since the salt mask is calculated using 32bit uint and it will overflow.
+        int m_saltBits = Math.min(31, 32 - m_tileBits - m_polyBits);
+        int saltMask = (1 << m_saltBits) - 1;
+        int tileMask = (1 << m_tileBits) - 1;
+        int polyMask = (1 << m_polyBits) - 1;
+        int salt = ((ref >> (m_polyBits + m_tileBits)) & saltMask);
+        int it = ((ref >> m_polyBits) & tileMask);
+        int ip = ref & polyMask;
+        return NavMesh.encodePolyId(salt, it, ip);
+    }
 }
--- detour/src/main/java/org/recast4j/detour/io/MeshSetWriter.java
+++ detour/src/main/java/org/recast4j/detour/io/MeshSetWriter.java
@@ -1,5 +1,5 @@
 /*
-Recast4J Copyright (c) 2015 Piotr Piastucki piotr@jtilia.org
+Recast4J Copyright (c) 2015-2018 Piotr Piastucki piotr@jtilia.org
 
 This software is provided 'as-is', without any express or implied
 warranty.  In no event will the authors be held liable for any damages
@@ -27,42 +27,52 @@ import org.recast4j.detour.NavMesh;
 
 public class MeshSetWriter extends DetourWriter {
 
-	private final MeshDataWriter writer = new MeshDataWriter();
-	private final NavMeshParamWriter paramWriter = new NavMeshParamWriter();
+    private final MeshDataWriter writer = new MeshDataWriter();
+    private final NavMeshParamWriter paramWriter = new NavMeshParamWriter();
 
-	public void write(OutputStream stream, NavMesh mesh, ByteOrder order, boolean cCompatibility) throws IOException {
-		// Write header.
-		write(stream, NavMeshSetHeader.NAVMESHSET_MAGIC, order);
-		write(stream, cCompatibility ? NavMeshSetHeader.NAVMESHSET_VERSION : NavMeshSetHeader.NAVMESHSET_VERSION_RECAST4J, order);
-		int numTiles = 0;
-		for (int i = 0; i < mesh.getMaxTiles(); ++i) {
-			MeshTile tile = mesh.getTile(i);
-			if (tile == null || tile.data == null || tile.data.header == null)
-				continue;
-			numTiles++;
-		}
-		write(stream, numTiles, order);
-		paramWriter.write(stream, mesh.getParams(), order);
+    public void write(OutputStream stream, NavMesh mesh, ByteOrder order, boolean cCompatibility) throws IOException {
+        writeHeader(stream, mesh, order, cCompatibility);
+        writeTiles(stream, mesh, order, cCompatibility);
+    }
 
-		// Store tiles.
-		for (int i = 0; i < mesh.getMaxTiles(); ++i) {
-			MeshTile tile = mesh.getTile(i);
-			if (tile == null || tile.data == null || tile.data.header == null)
-				continue;
+    private void writeHeader(OutputStream stream, NavMesh mesh, ByteOrder order, boolean cCompatibility) throws IOException {
+        write(stream, NavMeshSetHeader.NAVMESHSET_MAGIC, order);
+        write(stream, cCompatibility ? NavMeshSetHeader.NAVMESHSET_VERSION : NavMeshSetHeader.NAVMESHSET_VERSION_RECAST4J, order);
+        int numTiles = 0;
+        for (int i = 0; i < mesh.getMaxTiles(); ++i) {
+            MeshTile tile = mesh.getTile(i);
+            if (tile == null || tile.data == null || tile.data.header == null) {
+                continue;
+            }
+            numTiles++;
+        }
+        write(stream, numTiles, order);
+        paramWriter.write(stream, mesh.getParams(), order);
+        if (!cCompatibility) {
+            write(stream, mesh.getMaxVertsPerPoly(), order);
+        }
+    }
 
-			NavMeshTileHeader tileHeader = new NavMeshTileHeader();
-			tileHeader.tileRef = mesh.getTileRef(tile);
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			writer.write(baos, tile.data, order, cCompatibility);
-			byte[] ba = baos.toByteArray();
-			tileHeader.dataSize = ba.length;
-			write(stream, tileHeader.tileRef, order);
-			write(stream, tileHeader.dataSize, order);
-			if (cCompatibility) {
-				write(stream, 0, order); // C struct padding
-			}
-			stream.write(ba);
-		}
-	}
+    private void writeTiles(OutputStream stream, NavMesh mesh, ByteOrder order, boolean cCompatibility) throws IOException {
+        for (int i = 0; i < mesh.getMaxTiles(); ++i) {
+            MeshTile tile = mesh.getTile(i);
+            if (tile == null || tile.data == null || tile.data.header == null) {
+                continue;
+            }
+
+            NavMeshTileHeader tileHeader = new NavMeshTileHeader();
+            tileHeader.tileRef = mesh.getTileRef(tile);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            writer.write(baos, tile.data, order, cCompatibility);
+            byte[] ba = baos.toByteArray();
+            tileHeader.dataSize = ba.length;
+            write(stream, tileHeader.tileRef, order);
+            write(stream, tileHeader.dataSize, order);
+            if (cCompatibility) {
+                write(stream, 0, order); // C struct padding
+            }
+            stream.write(ba);
+        }
+    }
 
 }
