c3ffe191cce3197d46d83c2056e7488636f371cd
Piotr Piastucki <piotr@recast4j.org>
2023-03-16 06:51:46 +0100

    [Upstream] Fix for out of bounds read in tile cache builder

--- detour-tile-cache/src/main/java/org/recast4j/detour/tilecache/TileCacheBuilder.java
+++ detour-tile-cache/src/main/java/org/recast4j/detour/tilecache/TileCacheBuilder.java
@@ -1300,7 +1300,7 @@ public class TileCacheBuilder {
         }
 
         // Remove vertex.
-        for (int i = rem; i < mesh.nverts; ++i) {
+        for (int i = rem; i < mesh.nverts - 1; ++i) {
             mesh.verts[i * 3 + 0] = mesh.verts[(i + 1) * 3 + 0];
             mesh.verts[i * 3 + 1] = mesh.verts[(i + 1) * 3 + 1];
             mesh.verts[i * 3 + 2] = mesh.verts[(i + 1) * 3 + 2];
